17103611c226a4a2182f240a8fabb7722c2129ec
Jihong Park <40789010+VivonneBay404@users.noreply.github.com>
2023-01-31 01:04:11 +0900

    add pagination

--- src/pages/sikdang/SikdangList.vue
+++ src/pages/sikdang/SikdangList.vue
@@ -1,25 +1,42 @@
 <template>
   <div>
-    <v-row>
-      <v-col cols="2">
-        <v-navigation-drawer permanent>
-          <SikdangSort
-            @click="onCategoryClicked"
-            :selectedCategories="selectedCategories"
-        /></v-navigation-drawer>
-      </v-col>
+    <v-content>
+      <v-row>
+        <v-col cols="2">
+          <v-navigation-drawer permanent>
+            <SikdangSort
+              @click="onCategoryClicked"
+              :selectedCategories="selectedCategories"
+          /></v-navigation-drawer>
+        </v-col>
 
-      <v-col>
-        <v-row>
-          <v-col v-for="(sikdang, index) in sikdangs" :key="index">
-            <SikdangCard
-              v-bind="sikdang"
-              @click="onSikdangClicked(sikdang._id)"
-            />
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+        <v-col>
+          <v-row v-if="sikdangs.length">
+            <v-col
+              v-for="(sikdang, index) in sikdangs"
+              :key="index"
+              cols="12"
+              sm="6"
+              md="5"
+              lg="3"
+            >
+              <SikdangCard
+                v-bind="sikdang"
+                @click="onSikdangClicked(sikdang._id)"
+              />
+            </v-col>
+          </v-row>
+          <v-row v-else><h2>찾는 식당이 없습니다😱</h2></v-row>
+        </v-col>
+      </v-row>
+      <div class="text-center">
+        <v-pagination
+          v-model="page"
+          :length="parseInt(count / limit) + 1"
+          total-visible="7"
+        ></v-pagination>
+      </div>
+    </v-content>
   </div>
 </template>
 
@@ -37,11 +54,17 @@ export default {
     return {
       sikdangs: [],
       selectedCategories: [],
+      page: 1,
+      count: null,
+      limit: 5,
     };
   },
   methods: {
     onSikdangClicked(id) {
-      this.$router.push(`/sikdang/${id}`);
+      // this.$router.push(`/sikdang/${id}`);
+      //새로운 탭으로 열기
+      let route = this.$router.resolve({ path: `/sikdang/${id}` });
+      window.open(route.href, "_blank");
     },
     onCategoryClicked(id) {
       if (id === "전체") {
@@ -54,18 +77,34 @@ export default {
             )
           : this.selectedCategories.push(id);
       }
+      this.page = 1;
+    },
+    async getSikdang() {
+      const query = { category: this.selectedCategories, page: this.page };
+      console.log("query", query);
+      const data = (
+        await this.$axios.get("/sikdang", {
+          params: query,
+        })
+      ).data;
+      this.sikdangs = data.sikdang;
+      //해당 카테고리의 총합
+      this.count = data.count;
+      this.$router.push({ query: query }).catch(() => {});
+      console.log("router push ok");
     },
   },
   async mounted() {
     // console.log(dummyData);
     console.log("this.$route.query;", this.$route.query);
-
+    //선택된 카테고리가 하나면 오브젝트로 들어옴
     if (this.$route.query.category) {
       Array.isArray(this.$route.query.category)
         ? (this.selectedCategories = this.$route.query.category)
         : (this.selectedCategories = [this.$route.query.category]);
     }
 
+    this.getSikdang();
     // this.sikdangs = (
     //   await this.$axios.get("/sikdang", {
     //     params: this.$route.query,
@@ -75,16 +114,11 @@ export default {
   watch: {
     async selectedCategories() {
       console.log(" this.selectedCategories ", this.selectedCategories);
-      this.sikdangs = (
-        await this.$axios.get("/sikdang", {
-          params: { category: this.selectedCategories },
-        })
-      ).data;
+      this.getSikdang();
       //selectedCategories가 변하면 route query에 push
-      this.$router
-        .push({ query: { category: this.selectedCategories } })
-        .catch(() => {});
-      console.log("router push ok");
+    },
+    async page() {
+      this.getSikdang();
     },
   },
 };
